b2d608e794716d4f8e2e28d16a730aeba7b3f2ee
Efstathios Ignatakis <ignatakisefstathios@gmail.com>
2026-03-01 23:45:10 +0200

    Wrap app in MiniKitProvider for integration

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { WagmiProvider } from 'wagmi'
+import { MiniKitProvider } from '@worldcoin/minikit-react'
 import { config } from './lib/wagmi-config'
 import App from './App'
 import './index.css'
@@ -18,12 +19,14 @@ const queryClient = new QueryClient({
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </QueryClientProvider>
-    </WagmiProvider>
+    <MiniKitProvider appId={import.meta.env.VITE_APP_ID}>
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </QueryClientProvider>
+      </WagmiProvider>
+    </MiniKitProvider>
   </React.StrictMode>,
 )
