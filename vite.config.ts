e44e222cd48343c354aa75438618ef5cb185b49a
Efstathios Ignatakis <ignatakisefstathios@gmail.com>
2026-03-03 13:31:56 +0200

    Refactor vite.config.ts by removing unused settings
    Removed esbuild configuration and optimized dependencies settings.

--- vite.config.ts
+++ vite.config.ts
@@ -3,30 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    include: [/node_modules/],
-    exclude: [],
-    target: 'esnext'
-  },
   optimizeDeps: {
-    include: [
-      '@worldcoin/minikit-react',
-      '@worldcoin/minikit-js',
-      '@worldcoin/minikit-react/**/*',
-      '@worldcoin/minikit-js/**/*'
-    ],
-    force: true
-  },
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true,
-      include: [/node_modules/]
-    },
-    rollupOptions: {
-      external: [],
-      output: {
-        manualChunks: undefined
-      }
-    }
+    include: ['@worldcoin/minikit-js']
   }
-})'
+})
