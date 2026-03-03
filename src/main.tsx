e925373b058a3b9b460d43d8d6961cc3ab3ec99e
Efstathios Ignatakis <ignatakisefstathios@gmail.com>
2026-03-03 13:24:07 +0200

    Update MiniKitProvider import path

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { WagmiProvider } from 'wagmi'
-import { MiniKitProvider } from '@worldcoin/minikit-react'
+import { MiniKitProvider } from "@worldcoin/minikit-js/minikit-provider"
 import { config } from './lib/wagmi-config'
 import App from './App'
 import './index.css'
